aeae948ed995f2277647122f41dac17cd21d97ae
Froguin99 <b8008458@newcastle.ac.uk>
2022-06-08 11:51:26 +0100

    Added Test datasets and updated route calculation script

--- Processing/Scripts/SpatailDataToNetworkConversion.py
+++ Processing/Scripts/SpatailDataToNetworkConversion.py
@@ -37,6 +37,8 @@ plt.show()
 
 #%%
 
+# convert shapefile to nodes and edges
+
 nodes, edges = momepy.nx_to_gdf(momepy.gdf_to_nx(network_edges.explode()))
 nodes = nodes.set_index("nodeID")
 
@@ -49,18 +51,26 @@ ax.set_axis_off()
 plt.show()
 
 #%%
+
+# check for poor topology
 network_edges.plot(greedy(network_edges), categorical=True, figsize=(10, 10), cmap="Set3").set_axis_off()
 
 
 #%%
+
+# convert to graph 
 graph = momepy.gdf_to_nx(edges, approach='primal', length='mm_len')
 
 
 #%%
+
+# plot network as a graph 
 nx.draw(graph, node_size=100)
 print("This graph was drawn using network x's plotting function, and will come out as a random shape every time")
 
 #%%
+
+# plot barriers on network
 f, ax = plt.subplots(figsize=(10, 10))
 nodes.plot(ax=ax, zorder=2, label="network nodes")
 barriers.plot(ax=ax, zorder=3, label="Barriers")
@@ -71,30 +81,37 @@ plt.show()
 
 
 #%%
+
+# plot network, graph and overlayed
 f, ax = plt.subplots(1, 3, figsize=(18, 6), sharex=True, sharey=True)
 network_edges.plot(color='#e32e00', ax=ax[0])
 for i, facet in enumerate(ax):
-    facet.set_title(("NCN", "Primal graph", "Overlay")[i])
+    facet.set_title(("Geographic network", "Primal graph", "Overlay")[i])
     facet.axis("off")
 nx.draw(graph, {n:[n[0], n[1]] for n in list(graph.nodes)}, ax=ax[1], node_size=15)
 network_edges.plot(color='#e32e00', ax=ax[2], zorder=-1)
 nx.draw(graph, {n:[n[0], n[1]] for n in list(graph.nodes)}, ax=ax[2], node_size=15)
 
 #%%
+
+# calculate node degree
 graph = momepy.node_degree(graph, name='degree')
 
 
 #%%
 
+# convert to pandana network
 net = pdna.Network(nodes.geometry.x, nodes.geometry.y, edges["node_start"], edges["node_end"], edges[["Shape_Leng"]])
 
 #%%
 
+# create a reversed list of barriers in order to make a OD list
 barriers_reversed = barriers.iloc[::-1].reset_index()
 
 
 #%%
 
+# set origins and destinataions
 origins = net.get_node_ids(barriers.geometry.x, barriers.geometry.y).values
 destinations = net.get_node_ids(barriers_reversed.geometry.x, barriers_reversed.geometry.y).values
 
@@ -104,10 +121,40 @@ destinations_df = pd.DataFrame(destinations)
 
 #%%
 
-routes = []
+# import itertools package
+# create list of all combinations
+import itertools
+OD_pairs = list(itertools.product(origins,destinations))
 
-net.shortest_paths(origins, destinations)
+
 
 
 #%%
-barriers_shortest_paths = net.shortest_paths(barriers, barriers)
+
+
+# find all routes and all route distances 
+routes = []
+distances = []
+
+for a,b in OD_pairs:
+    route = net.shortest_paths(a, b)
+    routes.append(route)
+    distance = net.shortest_path_lengths(a, b, imp_name='Shape_Leng')
+    distances.append(distance)
+
+
+
+
+
+#%%
+
+# ALL CODE BEYOND THIS POINT IS STILL VERY MUCH A WORK IN PROGRESS
+
+edges['edge_pairs'] = edges['node_start'].astype(str) + ',' + edges['node_end'].astype(str)
+
+
+#%%
+
+if routes['Value'].str[0] == edges['node_start']:
+    if routes['Value'].str[last value in arrary]:
+        edges['route'] = routes['value']
